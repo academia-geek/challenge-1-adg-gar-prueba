e307b71cd746f57aee776a5dcc2795bac1d4f260
anderson <adguerreroc@unal.edu.co>
2020-05-02 18:13:23 -0500

    parte mobile finalizada- proyecto completado

--- js/script.js
+++ js/script.js
@@ -8,21 +8,43 @@ boton.addEventListener('click',(e)=>{
    if(text.length==0){
     document.getElementById("msjalert").innerHTML="Please provide a valid email";
     var eml =document.getElementById("email");
+    styleEml=window.getComputedStyle(eml);
+    emlWidth=styleEml.getPropertyValue('width');
+    if(emlWidth=='350px'){
     eml.style.border='2px solid hsl(0, 93%, 68%, 0.8)';
     eml.style.backgroundImage='url("./images/icon-error.svg")';
     eml.style.backgroundRepeat='no-repeat';
-    eml.style.backgroundPositionX="280px";
-    eml.style.backgroundPositionY="6px";
+    eml.style.backgroundPositionX="312px";
+    eml.style.backgroundPositionY="10px";
+    }
+    else if(emlWidth=='260px'){
+    eml.style.border='2px solid hsl(0, 93%, 68%, 0.8)';
+    eml.style.backgroundImage='url("./images/icon-error.svg")';
+    eml.style.backgroundRepeat='no-repeat';
+    eml.style.backgroundPositionX="200px";
+    eml.style.backgroundPositionY="10px";
+    }
 
    }
    else if(text.indexOf("@")==-1){
       document.getElementById("msjalert").innerHTML="Please provide a valid email";
       var eml =document.getElementById("email");
+      styleEml=window.getComputedStyle(eml);
+      emlWidth=styleEml.getPropertyValue('width');
+      if(emlWidth=='350px'){
       eml.style.border='2px solid hsl(0, 93%, 68%, 0.8)';
       eml.style.backgroundImage='url("./images/icon-error.svg")';
       eml.style.backgroundRepeat='no-repeat';
-      eml.style.backgroundPositionX="300px";
-      eml.style.backgroundPositionY="6px";
+      eml.style.backgroundPositionX="312px";
+      eml.style.backgroundPositionY="10px";
+      }
+      else if(emlWidth=='260px'){
+      eml.style.border='2px solid hsl(0, 93%, 68%, 0.8)';
+      eml.style.backgroundImage='url("./images/icon-error.svg")';
+      eml.style.backgroundRepeat='no-repeat';
+      eml.style.backgroundPositionX="200px";
+      eml.style.backgroundPositionY="10px";
+      }
    }
    else{
       document.getElementById("email").value="";
